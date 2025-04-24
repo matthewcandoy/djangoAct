0416203e218a5d7470b171f5ef0aa935dfbe8696
John Matthew Candoy <mjmcandoy@tip.edu.ph>
2025-04-24 18:35:46 +0800

    add build script and modify requirements

--- core/settings.py
+++ core/settings.py
@@ -9,23 +9,27 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os 
+import dj_database_url
+from pathlib import Path 
+ 
+from dotenv import load_dotenv 
+ 
+# Build paths inside the project like this: BASE_DIR / 'subdir'. 
+BASE_DIR = Path(__file__).resolve().parent.parent 
+ 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m10c@k!u5b!y@=n%!9dxmc4#=q)q$)tdu$6$&w#1p_y107=2c_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# SECURITY WARNING: keep the secret key used in production secret! 
+SECRET_KEY = os.getenv('SECRET_KEY') 
+ 
+# SECURITY WARNING: don't run with debug turned on in production! 
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1'] 
+ 
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ') 
 
 
 # Application definition
@@ -76,11 +80,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = { 
+'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600), 
 }
 
 
